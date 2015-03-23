889c05812063ee2316c6586b9b896c5ab80a798a
BBS007 <benji@benji-virtual-machine.(none)>
2015-03-23 20:15:22 +0100

    Added truc.h

---
+++ tp2/src/truc.h
@@ -0,0 +1,17 @@
+/*  truc.h
+  Header
+
+  -----| DERECURSIFICATION DE FONCTION PAR PILE |-----
+
+  BARBESANGE Benjamin,
+  PISSAVY Pierre-Loup
+
+  ISIMA 1ere Annee, 2014-2015
+*/
+
+#include <stio.h>
+#include <stlib.h>
+
+int TRUC(int , int );
+
+int truc_iter(int , int );
