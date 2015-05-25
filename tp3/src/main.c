5a15f06e5efefd60ccbd22756ed01125c9a10f4d
theCmaker <pierre-loup.pissavy94@orange.fr>
2015-05-25 15:01:32 +0200

    Added test function and enhanced main for reading a command file

--- tp3/src/main.c
+++ tp3/src/main.c
@@ -9,44 +9,105 @@
   ISIMA 1ere Annee, 2014-2015
 */
 
-#include "tree.h"
+void test();
 
-int main(/*int argc, char *argv[]*/) {
+#include "tree.h"
+int main(int argc, char *argv[]) {
+  FILE *f;
+  char buf[400];
+  char text[400];
+  tree_t *arbre = NULL;
+  if (argc > 1) {
+    /* Lecture de fichier de commandes */
+    f = fopen(argv[1],"r");
+    if (f) {
+      while (!feof(f)) {
+        buf[0] = '\0';
+        text[0] = '\0';
+        fgets(buf,400,f);
+        switch (buf[0]) {
+          case 'C': /* creer */
+            sscanf(&buf[1],"%s",text);
+            creerArbre(text,&arbre);
+            break;
+          case 'I': /* inserer */
+            sscanf(&buf[1],"%s",text);
+            insererMot(&arbre,text);
+            break;
+          case 'M': /* motif */
+            sscanf(&buf[1],"%s",text);
+            rech_motif(&arbre,text);
+            break;
+          case 'L': /* liberer */
+            if (arbre) {
+              libererArbre(&arbre);
+            } else {
+              printf("Liberation impossible\n");
+            }
+            break;
+          case 'A': /* afficher */
+            afficherArbre(arbre);
+            break;
+          case '#': /* texte */
+            printf("%s",&buf[1]);
+            break;
+          default:
+            puts("\n");
+        }
+      }
+      if (arbre) {
+        libererArbre(&arbre);
+      }
+      fclose(f);
+    } else {
+      fprintf(stderr, "Fichier invalide\n");
+    }
+  } else {
+    /* Fonctions de tests de base */
+    test();
+  }
+  return 0;
+}
+
+void test() {
   tree_t *monArbre = NULL;
   int i, nbMotifs = 4;
-  char **motif;
+  char *motif[] = {"a","","az","x"};
+  printf("##########\nDEBUT DU PROGRAMME DE TEST\n##########\n");
   
-  motif = (char**)malloc(nbMotifs * sizeof(char*));
-  motif[0] = "";
-  motif[1] = "a";
-  motif[2] = "az";
-  motif[3] = "x";
-  
-  if (creerArbre("(a(b(a(T))r(b(r(E))T(S)))b(a(i(L(l(e(R))))R(r(e)))i(e(N))o(s(s(E,U))))f(a(i(M,r(E))))j(o(u(e(T,u(R)))))k(i(m(o(n(O)))w(I)))m(a(S(s(E))))v(a(i(N)))y(a(c(K)))z(e(b(U))))",&monArbre)) {
+  if (creerArbre("(a(l(p(h(A))))b(r(a(v(E,O))))c(h(a(r(l(i(E))))))d(e(l(t(A(p(l(a(n(E)))))))))e(c(h(O))p(i(n(E))))g(o(l(F)))h(o(t(e(L))))i(n(d(i(A,e(N))))))",&monArbre)) {
     printf("**********\nAffichage avant insertion\n");
     afficherArbre(monArbre);
-    insererMot(&monArbre,"aba"); 
-    insererMot(&monArbre,"abattu");
-    insererMot(&monArbre,"Arbore");   /*Debut deja present */
-    insererMot(&monArbre,"artiSte");
-    insererMot(&monArbre,"clIent");   /* Aucune lettre deja presente */
-    insererMot(&monArbre,"arT");      /* Mot deja present */
+
+    insererMot(&monArbre,"ALPHABET");   /* Debut deja present */
+    insererMot(&monArbre,"foxtrot");   /* Aucune lettre deja presente */
+    insererMot(&monArbre,"echo");      /* Mot deja present */
+    insererMot(&monArbre,"epi");     /* Mot inclus dans un mot deja present */
     insererMot(&monArbre,"");         /* Mot vide */
-    insererMot(&monArbre,"joue");     /* Mot inclus dans un mot deja present */
-    insererMot(&monArbre,"kin");
-    insererMot(&monArbre,"papa");
     
     printf("**********\nAffichage apres insertion\n");
     afficherArbre(monArbre);
   	
-	for(i=0; i<nbMotifs; i++) {
+  	for (i = 0; i < nbMotifs; ++i) {
       printf("**********\nRecherche du motif \"%s\"\n", motif[i]);
       rech_motif(&monArbre, motif[i]);
-	}
+  	}
 
+    printf("**********\nInsertion dans l'arbre vide\n");
     libererArbre(&monArbre);
+    insererMot(&monArbre,"alpha");
+
+    printf("**********\nAffichage apres insertion\n");
+    afficherArbre(monArbre);
+
+    printf("**********\nLiberation de l'arbre\n");
+    libererArbre(&monArbre);
+    
+    printf("**********\nAffichage apres liberation\n");
+    afficherArbre(monArbre);
+
+    printf("##########\nFIN DU PROGRAMME DE TEST\n##########\n");
   } else {
     fprintf(stderr, "Probleme creation arbre\n");
   }
-  return 0;
 }
